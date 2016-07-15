2718337c1ef27c27bbcd27b409150d3326a7f622
psychoiguana <psychoiguana3@gmail.com>
2016-07-14 21:33:20 -0400

    doot

--- pages/registerpage/client/registerpage.js
+++ pages/registerpage/client/registerpage.js
@@ -1,11 +1,18 @@
 if (Meteor.isClient) {
-    Template.register.events({
+    Template.registerpage.events({
         'submit form': function(event) {
             event.preventDefault();
             var usernameVar = event.target.registerUsername.value;
             var emailVar = event.target.registerEmail.value;
             var passwordVar = event.target.registerPassword.value;
+            var confPasswordVar = event.target.registerConfPassword.value;
+            if (passwordVar == confPasswordVar) {
             console.log("Form submitted.");
+        }
+        	else {
+        		document.getElementById("failMsg").innerHTML = "Passwords do not match. Try again.";
+        		console.log("fail okay");
+        	}
         }
     });
     
