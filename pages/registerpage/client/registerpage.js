e314e866f8752bb227357b8d9338c75ee3e6475a
psychoiguana <psychoiguana3@gmail.com>
2016-07-15 10:23:31 -0400

    doot

--- pages/registerpage/client/registerpage.js
+++ pages/registerpage/client/registerpage.js
@@ -8,12 +8,36 @@ if (Meteor.isClient) {
             var confPasswordVar = event.target.registerConfPassword.value;
             if (passwordVar == confPasswordVar) {
             console.log("Form submitted.");
+            const email = emailVar;
+            const username = usernameVar;
+            const password = passwordVar;
+            const isDriver = false;
+            const isPassenger = false;
+            const name = null;
+            const zipCode = null;
+            const dateOfBirth = null;
+            const licensePlate = null;
+            const licenseNumber = null;
+            const phoneNumber = null;
+            const rating = null;
+            console.log(email);console.log(password);console.log(username);
+            const item = {email, username, password, isDriver, isPassenger, name, zipCode, dateOfBirth, licensePlate, licenseNumber, phoneNumber, rating};
+            console.dir(item);
+            Accounts.insert(item);     
         }
         	else {
         		document.getElementById("failMsg").innerHTML = "Passwords do not match. Try again.";
         		console.log("fail okay");
         	}
         }
+
+
     });
+    Template.registerpage.helpers({
+        users: function() {
+            return Accounts.find();
+        }
+
+    })
     
 }
