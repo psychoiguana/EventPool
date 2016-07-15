01a462493c313bf4ffe6fb420c3bc2bec8aa60cf
psychoiguana <psychoiguana3@gmail.com>
2016-07-15 14:51:32 -0400

    doot

--- pages/signinpage/client/signinpage.js
+++ pages/signinpage/client/signinpage.js
@@ -5,18 +5,7 @@ Template.signinpage.events({
         var usernameVar = event.target.signinpageUsername.value;
 
         var passwordVar = event.target.signinpagePassword.value;
-        {{#each user in users}} {
-        	if (user.username == usernameVar) {
-        		if (users.password == passwordVar) {
-
-        		}
-        		else {
-        			document.getElementById("youFailed").innerHTML = "Wrong Password, try again";
-        		}
-        	}
-		}
-		{{/each}}
-		document.getElementById("youFailed").innerHTML = "User does not exist, please try again"
+		
 
         console.log("Form submitted")
       }
