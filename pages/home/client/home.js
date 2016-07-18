053f1b43bc6cc387071fd7324eb92da9b082fd2d
Alina Zheng <bobicusj@gmail.com>
2016-07-18 16:36:34 -0400

    hi

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -9,3 +9,20 @@ Template.home.events({
 	},
 
 })
+
+Template.home.events{(
+	"click. js-searchbar": function(event) {
+		const searchedEvent = $(". js-searchedEvent").val(); 
+		// looks at the search keywords
+		// customize searchedevent page with the matching keywords in the pools collection
+
+		// create an empty collection
+		// store your matching events in the empty collection
+		// present it in the searchedevent.html page
+		// clear the collection
+		Router.go(searchedevent);
+
+	}
+	)} 
+
+//
