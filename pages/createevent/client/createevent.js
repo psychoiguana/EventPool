29241e77d952f6cfa6ba159ebef33869ed55a670
Seungsoo Ryan Lee <ssryanleeis@gmail.com>
2016-07-18 19:54:13 -0400

    table

--- pages/createevent/client/createevent.js
+++ pages/createevent/client/createevent.js
@@ -1,7 +1,7 @@
 Template.createevent.events({
 	'click .js-submit': function(event){
        event.preventDefault();
-     
+
        const category = $(".js-category").val();
        const location = $(".js-location").val();
        const title = $(".js-title").val();
@@ -12,7 +12,7 @@ Template.createevent.events({
        const createdAt = new Date();
        const obj = {location,category,title,time,date,createdAt,driver,seats};
        console.dir(obj);
-       console.log("were on our way!!");
+       console.log("we're on our way!!");
        console.log("Form submitted");
        Pools.insert(obj);
        Router.go("browse");
