b85f44a3ea408a6e884d2b45334968401b0e74d6
Seungsoo Ryan Lee <ssryanleeis@gmail.com>
2016-07-18 20:16:02 -0400

    renamed

--- pages/passengersetup/client/passengerregister.js
+++ pages/passengersetup/client/passengerregister.js
@@ -2,12 +2,12 @@ Template.passengerregister.events({
   "click .js-submit":  function(event){
             console.log("clicked it");
             event.preventDefault();
-            const  username = $(".js-name").val();
+            const Name = $(".js-name").val();
             const email = $(".js-email").val();
-	    const phone = $(".js-phone").val();
+	          const phone = $(".js-phone").val();
             const userId = Meteor.userId();
             console.log("Form submitted.");
-	    const obj = {username,email,phone,userId};
+	          const obj = {Name,email,phone,userId};
             console.dir(obj);
             Accounts.insert(obj);
             Router.go("home");
