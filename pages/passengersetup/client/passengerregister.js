225c51de5e181d17e0b1800f0ccd6fc567a71951
psychoiguana <psychoiguana3@gmail.com>
2016-07-19 10:19:40 -0400

    doot

--- pages/passengersetup/client/passengerregister.js
+++ pages/passengersetup/client/passengerregister.js
@@ -1,16 +1,19 @@
 Template.passengerregister.events({
   "click .js-submit":  function(event){
-            console.log("clicked it");
-            event.preventDefault();
-            const Name = $(".js-name").val();
-            const email = $(".js-email").val();
-	          const phone = $(".js-phone").val();
-            const userId = Meteor.userId();
-            console.log("Form submitted.");
-	          const obj = {Name,email,phone,userId};
-            console.dir(obj);
-            Accounts.insert(obj);
-            Router.go("home");
+        console.log("clicked it");
+        event.preventDefault();
+        const Name = $(".js-name").val();
+        const email = $(".js-email").val();
+	    const phone = $(".js-phone").val();
+        const isDriver = false;
+        const birthday = null;
+        const licensePlate = null;
+        const userId = Meteor.userId();
+        console.log("Form submitted.");
+	    const obj = {Name,email,phone,userId,isDriver,birthday,licensePlate};
+        console.dir(obj);
+        Accounts.insert(obj);
+        Router.go("home");
    },
 
 });
